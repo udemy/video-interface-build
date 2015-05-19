a00dc4eabd834ec2753f8631e9da7c3b00ebcc0d
burakbostancioglu <burak@udemy.com>
2015-05-19 14:53:20 -0700

    alpha release 2

--- GruntFile.js
+++ GruntFile.js
@@ -13,20 +13,42 @@ module.exports = function(grunt) {
         "bower-install-simple": {
             options: {
                 color: true,
-                directory: "dest"
+                directory: "../dest",
+                cwd: "bower_components"
             },
             "prod": {
                 options: {
                     production: true
                 }
             }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'build/video-interface-build.min.css': ['dest/video.js/dist/video-js/video-js.css', 'dest/videojs-resolution-selector/video-quality-selector.css']
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    // includes files within path and its sub-directories
+                    {expand: true, flatten: true,  src: ['dest/video.js/dist/video-js/font/*'], dest: 'build/font'},
+                    {expand: true, flatten: true,  src: ['dest/video.js/dist/video-js/lang/*'], dest: 'build/lang'},
+                    {expand: true, flatten: true,  src: ['dest/video.js/dist/video-js/*.swf'], dest: 'build/'}
+                ]
+            }
         }
     });
 
     grunt.loadNpmTasks("grunt-bower-install-simple");
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     // Default task(s).
-    grunt.registerTask('default', ['bower-install-simple']);
-    grunt.registerTask('default', ['uglify:videojs']);
-
+    grunt.registerTask('default', ['bower-install-simple', 'uglify:videojs', 'cssmin', 'copy']);
 };
